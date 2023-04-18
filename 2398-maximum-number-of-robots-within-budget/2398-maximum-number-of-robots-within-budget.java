742f4cd2911fad12153f4aa106568f4f8c611b22
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-04-19 02:41:48 +0530

    Time: 779 ms (15.38%), Space: 52.5 MB (9.79%) - LeetHub

--- 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.java
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.java
@@ -1,24 +1,49 @@
 class Solution {
-    public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) {
-        Deque<Integer> q = new ArrayDeque<>();
-        int n = chargeTimes.length;
-        long s = 0;
-        int j = 0;
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            int a = chargeTimes[i], b = runningCosts[i];
-            while (!q.isEmpty() && chargeTimes[q.getLast()] <= a) {
-                q.pollLast();
-            }
-            q.offer(i);
-            s += b;
-            while (!q.isEmpty() && chargeTimes[q.getFirst()] + (i - j + 1) * s > budget) {
-                if (q.getFirst() == j) {
-                    q.pollFirst();
+    public int maximumRobots(int[] chargeTimes, int[] runningCosts, long budget) 
+    {
+        int n=chargeTimes.length;
+        long runningSum[]=new long[n];
+        runningSum[0]=0;
+        int i;
+        for(i=1;i<n;i++)
+        {
+            runningSum[i]=(long)(runningSum[i-1]+(long)runningCosts[i]);
+        }
+        int j,max=0,ans=0,pos=0,maxIndex=0,x=0,temp=0,tempIndex=0;
+        long sum=0;
+        for(i=0;i<n-ans;i++)
+        {
+            //max=chargeTimes[i];
+            sum=0;
+            if(maxIndex<i)
+            {
+                for(x=i;x<=i+ans;x++)
+                {
+                    if(chargeTimes[x]>temp)
+                    {
+                        temp=chargeTimes[x];
+                        tempIndex=x;
+                    }
                 }
-                s -= runningCosts[j++];
+                max=temp;
+                maxIndex=tempIndex;
+                temp=0;
             }
-            ans = Math.max(ans, i - j + 1);
+            for(j=(pos>i?pos:i);j<n;j++)
+            {
+                if(chargeTimes[j]>max)
+                {
+                    max=chargeTimes[j];
+                    maxIndex=j;
+                }
+                sum=(max)+((j-i+1)*(runningSum[j]-runningSum[i]+runningCosts[i]));
+                if(sum>budget)
+                {
+                    pos=j;
+                    break;
+                }
+            }
+            ans=ans>(j-i)?ans:(j-i);
         }
         return ans;
     }
