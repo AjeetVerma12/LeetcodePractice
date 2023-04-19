81ebee93abcdd74b4de18123de8e728d9b35382f
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-04-20 02:40:32 +0530

    Time: 2 ms (89.92%), Space: 52.5 MB (61.48%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
@@ -1,24 +1,7 @@
 class Solution {
     public int[][] sortTheStudents(int[][] score, int k) 
     {
-        int m=score.length;
-        int n=score[0].length;
-        int i,j,x,temp;
-        for(i=0;i<m-1;i++)
-        {
-            for(j=0;j<m-i-1;j++)
-            {
-                if(score[j][k]<score[j+1][k])
-                {
-                    for(x=0;x<n;x++)
-                    {
-                        temp=score[j][x];
-                        score[j][x]=score[j+1][x];
-                        score[j+1][x]=temp;
-                    }
-                }
-            }
-        }
+        Arrays.sort(score, (a, b) -> b[k] - a[k]);
         return score;
     }
 }
