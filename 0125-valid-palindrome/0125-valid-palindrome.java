3a6e3d9cbedeeb7cc7b1faf7da48962b976aee3a
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-05-20 17:07:12 +0530

    Time: 3 ms (97.94%), Space: 41.8 MB (100.00%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,32 +2,19 @@ class Solution {
     
     public boolean isPalindrome(String s) 
     {
-        String s1="";
-        int n=s.length();
-        int i;
-        char ch;
-        for(i=0;i<n;i++)
-        {
-            ch=s.charAt(i);
-            if((ch>96 && ch<123)||(ch>47 && ch<58))
-            {
-                s1=s1+ch;
-            }
-            else if(ch>64 && ch<91)
-            {
-                s1=s1+(char)(ch+32);
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (!Character.isLetterOrDigit(s.charAt(i))) {
+                ++i;
+            } else if (!Character.isLetterOrDigit(s.charAt(j))) {
+                --j;
+            } else if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            } else {
+                ++i;
+                --j;
             }
         }
-        boolean b=true;
-        n=s1.length();
-        for(i=0;i<=n/2;i++)
-        {
-            if(n>0 && s1.charAt(i)!=(s1.charAt(n-i-1)))
-            {
-                b=false;
-                break;
-            }
-        }
-        return b;
+        return true;
     }
 }
