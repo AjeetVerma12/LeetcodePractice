6620441c3a824c5e6e1e46b6b3a67013c596d8c0
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-05-20 16:50:41 +0530

    Time: 303 ms (27.33%), Space: 45 MB (5.62%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,5 @@
 class Solution {
     
-    public boolean checkPalindrome(String s1,int n)
-    {
-        if(n>=s1.length()/2)
-        {
-            return true;
-        }
-        if(s1.charAt(n)!=s1.charAt(s1.length()-n-1))
-        {
-            return false;
-        }
-        return checkPalindrome(s1,n+1);
-    }
-    
     public boolean isPalindrome(String s) 
     {
         String s1="";
@@ -31,7 +18,16 @@ class Solution {
                 s1=s1+(char)(ch+32);
             }
         }
-        System.out.print(s1);
-        return checkPalindrome(s1,0);
+        boolean b=true;
+        n=s1.length();
+        for(i=0;i<=n/2;i++)
+        {
+            if(n>0 && s1.charAt(i)!=(s1.charAt(n-i-1)))
+            {
+                b=false;
+                break;
+            }
+        }
+        return b;
     }
 }
