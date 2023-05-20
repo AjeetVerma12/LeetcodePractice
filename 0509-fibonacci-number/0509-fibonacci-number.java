b5f33c8d4bea0ac5a868cf508cf23363d823f881
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-05-20 17:32:00 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (70.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,12 @@
 class Solution {
     int sum=0;
     public int fib(int n) {
-        if(n==0)
-        {
-            return 0;
+        int a = 0, b = 1;
+        while (n-- > 0) {
+            int c = a + b;
+            a = b;
+            b = c;
         }
-        if(n==1)
-        {
-            return 1;
-        }
-        return sum+fib(n-1)+fib(n-2);
+        return a;
     }
 }
