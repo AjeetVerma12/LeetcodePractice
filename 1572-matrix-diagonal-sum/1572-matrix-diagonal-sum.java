38df3ed284ec7848893fb72b86ecbe0e188b6238
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-06-10 23:31:45 +0530

    Time: 0 ms (100.00%), Space: 44 MB (5.57%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,22 +1,20 @@
 class Solution {
     public int diagonalSum(int[][] mat) 
     {
-        int n=mat.length;
-        int i;
-        int total=0;
-        for(i=0;i<n;i++)
-        {
-            if(mat[i][i]!=0)
-            {
-                total+=mat[i][i];
-                mat[i][i]=0;
-            }
-            if(mat[i][n-i-1]!=0)
-            {
-                total+=mat[i][n-i-1];
-                mat[i][n-i-1]=0;
-            }
+        int n = mat.length;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Add elements from primary diagonal.
+            ans += mat[i][i];
+            // Add elements from secondary diagonal.
+            ans += mat[n - 1 - i][i];
         }
-        return total;
+        // If n is odd, subtract the middle element as its added twice.
+        if (n % 2 != 0) {
+            ans -= mat[n / 2][n / 2];
+        }
+
+        return ans;
     }
 }
