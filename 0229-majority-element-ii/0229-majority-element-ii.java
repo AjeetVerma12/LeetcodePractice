12eba77acc2ffed039cee67b5f5a8bfd1c9ac1b3
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-06-15 15:24:53 +0530

    Time: 2 ms (97.84%), Space: 47.4 MB (5.98%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,25 +2,57 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) 
     {
         int n=nums.length;
-        List<Integer> ans=new ArrayList<>();
-        Map<Integer,Integer> temp = new HashMap<Integer,Integer>();
-        int i;
-        for(i=0;i<n;i++)
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = Integer.MIN_VALUE;
+        int el2 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) 
         {
-            if(temp.containsKey(nums[i]))
+            if (cnt1 == 0 && el2 != nums[i]) 
             {
-                temp.put(nums[i],temp.get(nums[i])+1);
+                cnt1 = 1;
+                el1 = nums[i];
+            } 
+            else if (cnt2 == 0 && el1 != nums[i]) 
+            {
+                cnt2 = 1;
+                el2 = nums[i];
+            } 
+            else if (nums[i] == el1) 
+            {
+                cnt1++;
             }
-            temp.putIfAbsent(nums[i],1);
-            if(temp.get(nums[i])==(n/3)+1)
+            else if (nums[i] == el2)
             {
-                ans.add(nums[i]);
-                if(ans.size()==2)
-                {
-                    break;
-                }
+                cnt2++;
+            }
+            else 
+            {
+                cnt1--; 
+                cnt2--;
             }
         }
-        return ans;
+        List<Integer> ls = new ArrayList<>();
+        cnt1 = 0; cnt2 = 0;
+        for (int i=0; i<n; i++) 
+        {
+            if (nums[i] == el1)
+            {
+                cnt1++;
+            }
+            if (nums[i] == el2)
+            {
+                cnt2++;
+            }
+        }
+        int mini=(int)(n / 3) + 1;
+        if (cnt1 >= mini)
+        {
+            ls.add(el1);
+        }
+        if (cnt2 >= mini)
+        {
+            ls.add(el2);
+        }
+        return ls;
     }
 }
