fbf22037243034e21b74268dca745a35525fd5b1
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-06-15 23:46:49 +0530

    Time: 34 ms (96.30%), Space: 50.9 MB (39.45%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,52 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) 
     {
-        int n=nums.length;
         Arrays.sort(nums);
         List<List<Integer>> ans = new ArrayList<>();
-        int i,point1,point2;
-        for(i=0;i<n-2;i++)
-        {
-            if(i>0 && nums[i]==nums[i-1])
-            {
+        int n = nums.length;
+        for (int i = 0; i < n - 2 && nums[i] <= 0; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            point1=i+1;
-            point2=n-1;
-            for(;point1<point2;)
-            {
-                if(nums[point1]+nums[point2]==(-1*nums[i]))
-                {
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(nums[i]);
-                    temp.add(nums[point1]);
-                    temp.add(nums[point2]);
-                    ans.add(temp);
-                    point1++;
-                    point2--;
-                    for(;point1<point2;)
-                    {
-                        if(nums[point1]==nums[point1-1])
-                        {
-                            point1++;
-                        }
-                        if(nums[point2]==nums[point2+1])
-                        {
-                            point2--;
-                        }
-                        if(nums[point1]!=nums[point1-1] && nums[point2]!=nums[point2+1])
-                        {
-                            break;
-                        }
+            int j = i + 1, k = n - 1;
+            while (j < k) {
+                int x = nums[i] + nums[j] + nums[k];
+                if (x < 0) {
+                    ++j;
+                } else if (x > 0) {
+                    --k;
+                } else {
+                    ans.add(List.of(nums[i], nums[j++], nums[k--]));
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        ++j;
                     }
-                }
-                else if(nums[point1]+nums[point2]>(-1*nums[i]))
-                {
-                    point2--;
-                }
-                else
-                {
-                    point1++;
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        --k;
+                    }
                 }
             }
         }
