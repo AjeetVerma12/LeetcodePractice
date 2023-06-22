32df7e8b757cdccad6481fb5f5d61ccd02baa13e
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-06-23 02:10:11 +0530

    Time: 10 ms (92.18%), Space: 45.6 MB (52.92%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,42 +1,30 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int days) 
-    {
-        int left=Integer.MIN_VALUE;
-        int right=0;
-        for(int i:weights)
-        {
-            left=Math.max(left,i);
-            right+=i;
+    public int shipWithinDays(int[] weights, int days) {
+        int left = 0, right = 0;
+        for (int w : weights) {
+            left = Math.max(left, w);
+            right += w;
         }
-        int mid;
-        for(;left<right;)
-        {
-            mid=(left+right)/2;
-            if(calcDay(weights,mid)<=days)
-            {
-                right=mid;
-            }
-            else
-            {
-                left=mid+1;
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (check(mid, weights, days)) {
+                right = mid;
+            } else {
+                left = mid + 1;
             }
         }
         return left;
     }
-        
-    public int calcDay(int[] weights, int cap)
-    {
-        int n=weights.length;
-        int i,count=1,total=0;
-        for(i=0;i<n;i++)
-        {
-            total+=weights[i];
-            if(total>cap)
-            {
-                count++;
-                total=weights[i];
+
+    private boolean check(int mx, int[] weights, int days) {
+        int ws = 0, cnt = 1;
+        for (int w : weights) {
+            ws += w;
+            if (ws > mx) {
+                ws = w;
+                ++cnt;
             }
         }
-        return count;
+        return cnt <= days;
     }
 }
