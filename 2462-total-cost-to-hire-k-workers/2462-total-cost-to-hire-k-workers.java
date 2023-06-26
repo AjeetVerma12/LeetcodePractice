0c9e821f086586564d7ed8afa3b05650ad7ad651
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-06-26 17:12:06 +0530

    Time: 68 ms (50.27%), Space: 56.2 MB (75.69%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.java
@@ -1,38 +1,43 @@
 class Solution {
-    public long totalCost(int[] costs, int k, int candidates) 
-    {
-        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> {
+    public long totalCost(int[] costs, int k, int candidates) {
+        // The worker with the lowest cost has the highest priority, if two players has the
+        // same cost, break the tie by their indices (0 or 1).
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
             if (a[0] == b[0]) {
                 return a[1] - b[1];
             }
-            return a[0] - b[0];
-        });
-        int n = costs.length;
-        int i = candidates - 1, j = n - candidates;
-        for (int h = 0; h < candidates; ++h) {
-            q.offer(new int[] {costs[h], h});
+            return a[0] - b[0];});
+        
+        // Add the first k workers with section id of 0 and 
+        // the last k workers with section id of 1 (without duplication) to pq.
+        for (int i = 0; i < candidates; i++) {
+            pq.offer(new int[] {costs[i], 0});
         }
-        for (int h = n - candidates; h < n; ++h) {
-            if (h > i) {
-                q.offer(new int[] {costs[h], h});
-            }
+        for (int i = Math.max(candidates, costs.length - candidates); i < costs.length; i++) {
+            pq.offer(new int[] {costs[i], 1});
         }
-        long ans = 0;
-        while (k-- > 0) {
-            var e = q.poll();
-            int c = e[0], x = e[1];
-            ans += c;
-            if (x <= i) {
-                if (++i < j) {
-                    q.offer(new int[] {costs[i], i});
+
+        long answer = 0;
+        int nextHead = candidates;
+        int nextTail = costs.length - 1 - candidates;
+
+        for (int i = 0; i < k; i++) {
+            int[] curWorker = pq.poll();
+            int curCost = curWorker[0], curSectionId = curWorker[1];
+            answer += curCost;
+            
+            // Only refill pq if there are workers outside.
+            if (nextHead <= nextTail) {
+                if (curSectionId == 0) {
+                    pq.offer(new int[]{costs[nextHead], 0});
+                    nextHead++;
+                } else {
+                    pq.offer(new int[]{costs[nextTail], 1});
+                    nextTail--;
                 }
             }
-            if (x >= j) {
-                if (--j > i) {
-                    q.offer(new int[] {costs[j], j});
-                }
-            }
         }
-        return ans;
+
+        return answer;
     }
 }
