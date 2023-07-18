2288ad6c6eff7e8e286062dad6aa6b0c791d1fc9
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-07-18 15:00:03 +0530

    Time: 10 ms (94.04%), Space: 43.9 MB (60.00%) - LeetHub

--- 1125-smallest-sufficient-team/1125-smallest-sufficient-team.java
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.java
@@ -43,41 +43,5 @@ class Solution {
             ans.add(samp2[i]);
         }
         return ans.stream().mapToInt(Integer::intValue).toArray();
-        
-        // Map<String, Integer> d = new HashMap<>();
-        // int m = req_skills.length;
-        // int n = people.size();
-        // for (int i = 0; i < m; ++i) {
-        //     d.put(req_skills[i], i);
-        // }
-        // int[] p = new int[n];
-        // for (int i = 0; i < n; ++i) {
-        //     for (var s : people.get(i)) {
-        //         p[i] |= 1 << d.get(s);
-        //     }
-        // }
-        // int[] f = new int[1 << m];
-        // int[] g = new int[1 << m];
-        // int[] h = new int[1 << m];
-        // final int inf = 1 << 30;
-        // Arrays.fill(f, inf);
-        // f[0] = 0;
-        // for (int i = 0; i < 1 << m; ++i) {
-        //     if (f[i] == inf) {
-        //         continue;
-        //     }
-        //     for (int j = 0; j < n; ++j) {
-        //         if (f[i] + 1 < f[i | p[j]]) {
-        //             f[i | p[j]] = f[i] + 1;
-        //             g[i | p[j]] = j;
-        //             h[i | p[j]] = i;
-        //         }
-        //     }
-        // }
-        // List<Integer> ans = new ArrayList<>();
-        // for (int i = (1 << m) - 1; i != 0; i = h[i]) {
-        //     ans.add(g[i]);
-        // }
-        // return ans.stream().mapToInt(Integer::intValue).toArray();
     }
 }
