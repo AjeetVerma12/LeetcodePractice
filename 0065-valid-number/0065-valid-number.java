0e9f37284307ebc076e7ac4efb8c32efdcf69468
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-08-01 03:33:37 +0530

    Time: 1 ms (100.00%), Space: 41.6 MB (76.56%) - LeetHub

--- 0065-valid-number/0065-valid-number.java
+++ 0065-valid-number/0065-valid-number.java
@@ -14,23 +14,33 @@ class Solution {
             return false;
         }
         int dot = 0, e = 0;
-        for (int j = i; j < n; ++j) {
-            if (s.charAt(j) == '.') {
-                if (e > 0 || dot > 0) {
+        for (int j = i; j < n; ++j) 
+        {
+            if (s.charAt(j) == '.') 
+            {
+                if (e > 0 || dot > 0) 
+                {
                     return false;
                 }
                 ++dot;
-            } else if (s.charAt(j) == 'e' || s.charAt(j) == 'E') {
-                if (e > 0 || j == i || j == n - 1) {
+            } 
+            else if (s.charAt(j) == 'e' || s.charAt(j) == 'E') 
+            {
+                if (e > 0 || j == i || j == n - 1) 
+                {
                     return false;
                 }
                 ++e;
-                if (s.charAt(j + 1) == '+' || s.charAt(j + 1) == '-') {
-                    if (++j == n - 1) {
+                if (s.charAt(j + 1) == '+' || s.charAt(j + 1) == '-') 
+                {
+                    if (++j == n - 1) 
+                    {
                         return false;
                     }
                 }
-            } else if (s.charAt(j) < '0' || s.charAt(j) > '9') {
+            } 
+            else if (s.charAt(j) < '0' || s.charAt(j) > '9') 
+            {
                 return false;
             }
         }
