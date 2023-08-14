3dbbd484595584c6c8b674b2656fda752dc4a0a9
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-08-14 16:28:24 +0530

    Time: 69 ms (80.44%), Space: 58.1 MB (20.50%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,41 +1,19 @@
-class carState
-{
-    public int distance;
-    public int speed;
-    public double time;
-    
-    public carState(int distance, int speed, double time)
-    {
-        this.distance=distance;
-        this.speed=speed;
-        this.time=time;
-    }
-}
-
 class Solution {
     
     public int carFleet(int target, int[] position, int[] speed) {
-        int l=position.length;
-        carState cars[]=new carState[l];
-        int i;
-        for(i=0;i<l;i++)
-        {
-            cars[i]=new carState(target-position[i],speed[i],((double)(target-position[i]))/(double)(speed[i]));
+        int n = position.length;
+        Integer[] idx = new Integer[n];
+        for (int i = 0; i < n; ++i) {
+            idx[i] = i;
         }
-        Arrays.sort(cars, new Comparator<carState>() {
-            public int compare(carState state1, carState state2) {
-                return state1.distance-state2.distance;
-            }
-        });
-        
-        int ans=0;
-        double currTime=0.0;
-        for(i=0;i<cars.length;i++)
-        {
-            if(cars[i].time>currTime)
-            {
-                ans++;
-                currTime=cars[i].time;
+        Arrays.sort(idx, (i, j) -> position[j] - position[i]);
+        int ans = 0;
+        double pre = 0;
+        for (int i : idx) {
+            double t = 1.0 * (target - position[i]) / speed[i];
+            if (t > pre) {
+                ++ans;
+                pre = t;
             }
         }
         return ans;
