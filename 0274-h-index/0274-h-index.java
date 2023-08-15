f37c404c4ea3dfd115be97d0009e040f8c82f37c
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-08-16 02:44:35 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (38.15%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,43 +1,15 @@
 class Solution {
-    
-    private boolean possible(int[]citations, int n, int num)
-    {
-        int i;
-        int count=0;
-        for(i=0;i<n;i++)
-        {
-            if(citations[i]>=num)
-            {
-                count++;
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] cnt = new int[n + 1];
+        for (int x : citations) {
+            ++cnt[Math.min(x, n)];
+        }
+        for (int h = n, s = 0;; --h) {
+            s += cnt[h];
+            if (s >= h) {
+                return h;
             }
         }
-        if(count>=num)
-        {
-            return true;
-        }
-        return false;
-    }
-    
-    public int hIndex(int[] citations) 
-    {
-        int n=citations.length;
-        int left=0;
-        int right=n-1;
-        int ans=0;
-        int mid;
-        while(left<=right)
-        {
-            mid=(left+right)>>1;
-            if(possible(citations,n,mid+1))
-            {
-                ans=mid+1;
-                left=mid+1;
-            }
-            else
-            {
-                right=mid-1;
-            }
-        }
-        return ans;
     }
 }
