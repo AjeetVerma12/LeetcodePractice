40af97e2f86e22d6f68aa5f5f28dca86c51e5535
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-08-23 02:18:30 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (91.09%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -1,11 +1,23 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
-        String ans = "";
-        while (columnNumber != 0) {
-            --columnNumber;
-            ans=((char) ('A' + columnNumber % 26))+ans;
-            columnNumber /= 26;
+        // String ans = "";
+        // while (columnNumber != 0) {
+        //     --columnNumber;
+        //     ans=((char) ('A' + columnNumber % 26))+ans;
+        //     columnNumber /= 26;
+        // }
+        // return ans;
+        
+        StringBuilder ans = new StringBuilder();
+        
+        while(columnNumber > 0) {
+            columnNumber--;
+            // Get the last character and append it at the end of the string.
+            ans.append((char)((columnNumber) % 26 + 'A'));
+            columnNumber = (columnNumber) / 26;
         }
-        return ans;
+        
+        // Reverse it, as we appended characters in reverse order.
+        return ans.reverse().toString();
     }
 }
