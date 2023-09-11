e02abe5f6eb1f6eaf43f87ab0ae46ffab5f08122
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-09-12 02:37:44 +0530

    Time: 5 ms (93.74%), Space: 44.9 MB (9.00%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,39 +1,16 @@
 class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
-        int n=groupSizes.length;
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> group = new ArrayList<>();
-        HashMap<Integer,List<Integer>> map = new HashMap<>();
-        int i;
-        for(i=0;i<n;i++)
-        {
-            if(map.containsKey(groupSizes[i]))
-            {
-                List<Integer> temp=map.get(groupSizes[i]);
-                temp.add(i);
-                map.remove(groupSizes[i]);
-                map.put(groupSizes[i],temp);
-            }
-            else
-            {
-                List<Integer> temp=new ArrayList<>();
-                temp.add(i);
-                group.add(groupSizes[i]);
-                map.put(groupSizes[i],temp);
-            }
+        int n = groupSizes.length;
+        List<Integer>[] g = new List[n + 1];
+        Arrays.setAll(g, k -> new ArrayList<>());
+        for (int i = 0; i < n; ++i) {
+            g[groupSizes[i]].add(i);
         }
-        int j,k;
-        for(i=0;i<group.size();i++)
-        {
-            List<Integer> list=map.get(group.get(i));
-            for(j=0;j<list.size();)
-            {
-                List<Integer> temp=new ArrayList<>();
-                for(k=0;k<group.get(i);k++,j++)
-                {
-                    temp.add(list.get(j));
-                }
-                ans.add(temp);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < g.length; ++i) {
+            List<Integer> v = g[i];
+            for (int j = 0; j < v.size(); j += i) {
+                ans.add(v.subList(j, j + i));
             }
         }
         return ans;
