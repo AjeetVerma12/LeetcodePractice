31bb69d26b61a8079e1699dac26cb0097a57d45f
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-09-16 06:00:25 +0530

    Time: 1 ms (95.38%), Space: 45.5 MB (82.28%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div547-number-of-provinces/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div547-number-of-provinces.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div547-number-of-provinces/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div547-number-of-provinces.java
@@ -1,53 +1,27 @@
 class Solution {
-    private static boolean vis[];
-    private static void bfs(ArrayList<ArrayList<Integer>> adjLs, int node)
-    {
-        vis[node]=true;
-        Queue<Integer> q=new LinkedList<>();
-        q.add(node);
-        while(!q.isEmpty())
-        {
-            int temp=q.poll();
-            for(int it:adjLs.get(temp))
-            {
-                if(!vis[it])
-                {
-                    q.add(it);
-                    vis[it]=true;
-                }
-            }
-        }
-    }
-    
+    private int[][] g;
+    private boolean[] vis;
+
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        int i,n=isConnected.length,j;
-        for(i=0;i<n;i++)
-        {
-            adj.add(new ArrayList<Integer>()); 
-        }
-        for(i=0;i<n;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                if(isConnected[i][j]==1 && i!=j)
-                {
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
-        vis=new boolean[n];
-        int ans=0;
-        //int i;
-        for(i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                ans++;
-                bfs(adj,i);
+        g = isConnected;
+        int n = g.length;
+        vis = new boolean[n];
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (!vis[i]) {
+                dfs(i);
+                ++ans;
             }
         }
         return ans;
+    }
+
+    private void dfs(int i) {
+        vis[i] = true;
+        for (int j = 0; j < g.length; ++j) {
+            if (!vis[j] && g[i][j] == 1) {
+                dfs(j);
+            }
+        }
     }
 }
