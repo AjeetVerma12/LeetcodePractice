42be90366e9adcf330a51654758b48950afc0a45
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-09-17 05:12:53 +0530

    Time: 36 ms (96.91%), Space: 44.5 MB (30.98%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1631-path-with-minimum-effort.java
@@ -1,29 +1,62 @@
+class Pair
+{
+    int dist;
+    int row;
+    int col;
+    public Pair(int dist,int row,int col)
+    {
+        this.dist=dist;
+        this.row=row;
+        this.col=col;
+    }
+}
+
 class Solution {
-    public int minimumEffortPath(int[][] heights) {
-        int m = heights.length;
-        int n = heights[0].length;
-        int[][] dist = new int[m][n];
-        for (int i = 0; i < m; ++i) {
-            Arrays.fill(dist[i], 0x3f3f3f3f);
+    public int minimumEffortPath(int[][] heights) 
+    {
+        PriorityQueue<Pair> pq=new PriorityQueue<Pair>((x,y)-> x.dist-y.dist);
+        int m=heights.length;
+        int n=heights[0].length;
+        int dist[][]=new int[m][n];
+        int i,j;
+        int inf=(int)1e9;
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                dist[i][j]=inf;
+            }
         }
-        dist[0][0] = 0;
-        PriorityQueue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
-        q.offer(new int[] {0, 0, 0});
-        int[] dirs = {-1, 0, 1, 0, -1};
-        while (!q.isEmpty()) {
-            int[] p = q.poll();
-            int t = p[0], i = p[1], j = p[2];
-            for (int k = 0; k < 4; ++k) {
-                int x = i + dirs[k], y = j + dirs[k + 1];
-                if (x >= 0 && x < m && y >= 0 && y < n) {
-                    int nd = Math.max(t, Math.abs(heights[x][y] - heights[i][j]));
-                    if (nd < dist[x][y]) {
-                        dist[x][y] = nd;
-                        q.offer(new int[] {nd, x, y});
+        dist[0][0]=0;
+        pq.add(new Pair(0,0,0));
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        while(pq.size()!=0)
+        {
+            Pair it=pq.peek();
+            pq.remove();
+            int diff=it.dist;
+            int r=it.row;
+            int c=it.col;
+            if(r==(m-1) && c==(n-1))
+            {
+                return diff;
+            }
+            for(i=0;i<4;i++)
+            {
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n)
+                {
+                    int newEffort=Math.max(Math.abs(heights[r][c]-heights[nrow][ncol]),diff);
+                    if(newEffort<dist[nrow][ncol])
+                    {
+                        dist[nrow][ncol]=newEffort;
+                        pq.add(new Pair(newEffort,nrow,ncol));
                     }
                 }
             }
         }
-        return dist[m - 1][n - 1];
+        return 0;
     }
 }
