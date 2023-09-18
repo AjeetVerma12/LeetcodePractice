cb72b1d8f0826c22a021c9a01f9749ffb3574d45
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-09-18 13:11:13 +0530

    Time: 3 ms (44.39%), Space: 44.6 MB (59.35%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix.java
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence1-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,46 +1,26 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        int m=mat.length;
-        int n=mat[0].length;
-        int dp[][]=new int[m][2];
-        int i,j;
-        for(i=0;i<m;i++)
-        {
-            dp[i][1]=i;
-            for(j=0;j<n;j++)
-            {
-                if(mat[i][j]==0)
-                {
-                    break;
-                }
-                else
-                {
-                    dp[i][0]++;
+        int m = mat.length, n = mat[0].length;
+        int[] res = new int[m];
+        List<Integer> idx = new ArrayList<>();
+        for (int i = 0; i < m; ++i) {
+            idx.add(i);
+            int[] row = mat[i];
+            int left = 0, right = n;
+            while (left < right) {
+                int mid = (left + right) >> 1;
+                if (row[mid] == 0) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
                 }
             }
+            res[i] = left;
         }
-        
-        int temp;
-        for(i=0;i<m-1;i++)
-        {
-            for(j=0;j<m-i-1;j++)
-            {
-                if(dp[j][0]>dp[j+1][0])
-                {
-                    temp=dp[j][0];
-                    dp[j][0]=dp[j+1][0];
-                    dp[j+1][0]=temp;
-                    temp=dp[j][1];
-                    dp[j][1]=dp[j+1][1];
-                    dp[j+1][1]=temp;
-                }
-            }
-        }
-            
-        int ans[]=new int[k];
-        for(i=0;i<k;i++)
-        {
-            ans[i]=dp[i][1];
+        idx.sort(Comparator.comparingInt(a -> res[a]));
+        int[] ans = new int[k];
+        for (int i = 0; i < k; ++i) {
+            ans[i] = idx.get(i);
         }
         return ans;
     }
