335577600a99e03e38b616113cac0ddbf1fbb3cc
Ajeet Verma <94473170+AjeetVerma12@users.noreply.github.com>
2023-10-01 16:11:29 +0530

    Time: 9 ms (15.51%), Space: 44.6 MB (16.37%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div931-minimum-falling-path-sum.java
@@ -0,0 +1,64 @@
+class Solution {
+    
+    // int dp[][];
+    // private int recurSolve(int[][] matrix, int n, int row, int column,int[][] dp) {
+    //     if(column<0 || column >=n) {
+    //         return (int) Math.pow(10, 9);
+    //     }
+    //     if(row==0) {
+    //         return matrix[0][column];
+    //     }
+    //     if(dp[row][column]!=-1) {
+    //         return dp[row][column];
+    //     }
+    //     int bottom=matrix[row][column],bottomL=matrix[row][column],bottomR=matrix[row][column];
+    //     bottom+=recurSolve(matrix,n,row-1,column,dp);
+    //     bottomL+=recurSolve(matrix,n,row-1,column-1,dp);
+    //     bottomR+=recurSolve(matrix,n,row-1,column+1,dp);
+    //     return dp[row][column]=Math.min(bottom,Math.min(bottomL,bottomR));
+    // }
+    
+    public int minFallingPathSum(int[][] matrix) {
+        int n=matrix.length;
+        int[][] dp=new int[n][n];
+        // for(int row[]:dp) {
+        //     Arrays.fill(row,-1);
+        // }
+        // int max=Integer.MAX_VALUE;
+        // for(int i=0;i<n;i++) {
+        //     int temp=recurSolve(matrix,n,n-1,i,dp);
+        //     max=Math.min(max,temp);
+        // }
+        // return max;
+        
+        int i,j;
+        for(i=0;i<n;i++) {
+            dp[0][i]=matrix[0][i];
+        }
+        for(i=1;i<n;i++) {
+            for(j=0;j<n;j++) {
+                int up=matrix[i][j]+dp[i-1][j];
+                int upLeft=matrix[i][j];
+                if(j>0) {
+                    upLeft+=dp[i-1][j-1];
+                }
+                else {
+                    upLeft+=(int)Math.pow(10,9);
+                }
+                int upRight=matrix[i][j];
+                if(j<n-1) {
+                    upRight+=dp[i-1][j+1];
+                }
+                else {
+                    upRight+=(int)Math.pow(10,9);
+                }
+                dp[i][j]=Math.min(up,Math.min(upRight,upLeft));
+            }
+        }
+        int min=Integer.MAX_VALUE;
+        for(i=0;i<n;i++) {
+            min=Math.min(min,dp[n-1][i]);
+        }
+        return min;
+    }
+}
